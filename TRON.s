6ea7825f36051e968bb72177b6dfbd9db2754e80
Michael <junhyeok.hong@mail.utoronto.ca>
2018-03-31 12:53:54 -0400

    Added the KEYs for the Player One
    Added the KEYs for the Player One and so that in the interrupt, it will go to the corresponding w,a,s,d. And added double buffer

--- TRON.s
+++ TRON.s
@@ -2,7 +2,7 @@
 	 ####################
 	######################
 	##     			 	##
-	##	  TRON v1.0	 	##
+	##	  TRON v2.0	 	##
 	##			 		##
 	##	  Created by 	##
 	##	  Graham Sider	##
@@ -42,7 +42,20 @@
 	# (TEMP) LEDS
 	.equ LED_BASE, 0xFF200000			# STORED IN: r11
 	
-	
+    # Data for Player One
+    PlayerOneData:  .byte ' '           # STORE IN: r12
+    .equ KEY_A, 0x1C                    # All these keys stored in r7
+    .equ KEY_W, 0x1D
+    .equ KEY_S, 0x1B
+    .equ KEY_D, 0x23
+
+    # Data for Player Two
+    PlayerTwoData:  .byte ' '
+
+    # Colour Values For the Pixel buffer
+
+    .equ red, 0xF100
+    .equ blue, 0x001F
 	
 	
 	##############
@@ -64,9 +77,44 @@ _ps2_valid:								# PS2 DATA VALID
 	
 	andi r5, r4, 0xFFFF0000				# r5 = NUM CHARS LEFT TO READ
 	andi r6, r4, 0x000000FF				# r6 = DATA
-	stwio r6, 0(r11)					# DISPLAY DATA VIA LEDS
-	bne r5, r0, _ps2_valid				# IF MORE CHARS TO READ, RE-LOOP
-	subi ea, ea, 4
+
+    movi r7, KEY_A                      # r7 = A
+    beq r6, r7, _key_a_pressed
+
+    movi r7, KEY_W                      # r7 = W
+    beq r6, r7, _key_w_pressed
+
+    movi r7, KEY_S                      # r7 = S
+    beq r6, r7, _key_s_pressed
+
+    movi r7, KEY_D                      # r7 = D
+    beq r6, r7, _key_d_pressed
+
+    br _done_player_move                # branch to end exception handler
+
+_key_a_pressed:
+    movia r12, PlayerOneData            # Write the Key into memory
+    stbio r6, 0(r12)
+
+    br _done_player_move                # branch to end exception handler
+
+_key_w_pressed:
+
+    br _done_player_move                # branch to end exception handler
+
+_key_s_pressed:
+
+    br _done_player_move                # branch to end exception handler
+
+_key_d_pressed:
+
+    br _done_player_move                # branch to end exception handler
+
+_done_player_move:                      #END EXCEPTION HANDLER
+    stwio r6, 0(r11)                    # DISPLAY DATA VIA LEDS
+    bne r5, r0, _ps2_valid              # IF MORE CHARS TO READ, RE-LOOP
+    subi ea, ea, 4
+
 	eret								# DONE
 	
 	
@@ -95,9 +143,12 @@ _setup:
 	movia r9, VCB_BASE					# r9 = VCB BASE
 	movia r10, PS2_BASE					# r10 = PS2 BASE
 	movia r11, LED_BASE					# r11 = LED BASE (TEMP)
-	
+
+    # ENABLE DOUBLE BUFFER
+    movui  r4, 0x1                      # r4 = TEMP
+    stwio r4, VCB_FRONT_BUFFER(r9)      # Enable double buffer (swap pixel data from back buffer to front buffer)
+
 	# INTERRUPT SETUP
-	movui r4, 0x1						# r4 = TEMP
 	wrctl ctl0, r4						# CPU	(status)
 	stwio r4, PS2_CONTROL(r10)			# PS2	(IRQ 7)
 	movui r4, 0x7
