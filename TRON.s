9c6bba9dee066669f859bd46b48fa408df43de5a
grahamsider <37253861+grahamsider@users.noreply.github.com>
2018-04-08 22:28:11 -0400

    v5.0
    - Added start screen
    - Added message on player win
    - Added LED flash on player win
    - Finished up functionality

--- TRON.s
+++ TRON.s
@@ -2,7 +2,7 @@
 	 #####################
 	#######################
 	##					 ##
-	##	  TRON v4.1		 ##
+	##	  TRON v5.0		 ##
 	##					 ##
 	##	  Created by 	 ##
 	##	  Graham Sider	 ##
@@ -38,7 +38,13 @@
 	#PS/2 CONTROLLER
 	.equ PS2_BASE, 0xFF200100			# STORED IN: r9
 	.equ PS2_CONTROL, 0x04				# BASE + 4
+
+	# VGA CHARACTER BUFFER
+	.equ VGA_CHAR_BUFFER, 0x09000000
 	
+	# LED BASE
+	.equ LED_BASE, 0xFF200000
+
 	
 	### STORAGE ###
 	
@@ -59,14 +65,14 @@
 	.equ KEY_ARROW_D, 0x72
 	.equ KEY_ARROW_R, 0x74
 
+	# OTHER KEYS
+	.equ KEY_ENTER, 0x5A
+
 	# MOVEMENT VALS
 	.equ MOVE_RIGHT, 2
 	.equ MOVE_LEFT, -2
 	.equ MOVE_UP, -1024
 	.equ MOVE_DOWN, 1024
-	
-	# IN GAME OR SPLASHSCREEN
-	GAME_OR_SPLASH:	.byte ' '
 
     # COLOUR VALS
     .equ COLOUR_P1, 0xCB3A						# WAS 0xC87A
@@ -87,7 +93,30 @@
 	.equ SCREEN_BOTTOM_LEFT_COORDS, 0x3BC00
 	.equ SCREEN_BOTTOM_RIGHT_COORDS, 0x3BE7E
 
+    # HEX DISPLAY (HEX0 = PLAYER 1 & HEX1 = PLAYER 2)
+    .equ HEX_ADDRESS, 0xFF200020
+
+    .equ SCORE_ZERO_ZERO, 0x3F3F                # HEX0 = 0 & HEX1 = 0
+    .equ SCORE_ONE_ZERO, 0x063F                 # HEX0 = 1 & HEX1 = 0
+    .equ SCORE_TWO_ZERO, 0x5B3F                 # HEX0 = 2 & HEX1 = 0
+    .equ SCORE_THREE_ZERO, 0x4F3F               # HEX0 = 3 & HEX1 = 0
+
+    .equ SCORE_ZERO_ONE, 0x3F06                 # HEX0 = 0 & HEX1 = 1
+    .equ SCORE_ONE_ONE, 0x0606                  # HEX0 = 1 & HEX1 = 1
+    .equ SCORE_TWO_ONE, 0x5B06                  # HEX0 = 2 & HEX1 = 1
+    .equ SCORE_THREE_ONE, 0x4F06                # HEX0 = 3 & HEX1 = 1
+
+    .equ SCORE_ZERO_TWO, 0x3F5B                 # HEX0 = 0 & HEX1 = 2
+    .equ SCORE_ONE_TWO, 0x065B                  # HEX0 = 1 & HEX1 = 2
+    .equ SCORE_TWO_TWO, 0x5B5B                  # HEX0 = 2 & HEX1 = 2
+    .equ SCORE_THREE_TWO, 0x4F5B                # HEX0 = 3 & HEX1 = 2
+
+    .equ SCORE_ZERO_THREE, 0x3F4F               # HEX0 = 0 & HEX1 = 3
+    .equ SCORE_ONE_THREE, 0x064F                # HEX0 = 1 & HEX1 = 3
+    .equ SCORE_TWO_THREE, 0x5B4F                # HEX0 = 2 & HEX1 = 3
+    .equ SCORE_THREE_THREE, 0x4F4F              # HEX0 = 3 & HEX1 = 3
 	
+
 	
 	
 	##############
@@ -110,31 +139,38 @@ _ps2_valid:								# PS2 DATA VALID
 	ldwio et, 0(r9)						# et = DATA (TEMP)
 	andi et, et, 0x000000FF
 	
-	movui r23, KEY_A					# r23 = TEMP KEYS
+	movia r23, KEY_ENTER
+	beq et, r23, _key_enter
+
+	movia r23, KEY_A					# r23 = TEMP KEYS
 	beq et, r23, _p1_key_a
 	
-	movui r23, KEY_W
+	movia r23, KEY_W
 	beq et, r23, _p1_key_w
 	
-	movui r23, KEY_S
+	movia r23, KEY_S
 	beq et, r23, _p1_key_s
 	
-	movui r23, KEY_D
+	movia r23, KEY_D
 	beq et, r23, _p1_key_d
 	
-	movui r23, KEY_ARROW_L
+	movia r23, KEY_ARROW_L
 	beq et, r23, _p2_key_arrow_l
 	
-	movui r23, KEY_ARROW_U
+	movia r23, KEY_ARROW_U
 	beq et, r23, _p2_key_arrow_u
 	
-	movui r23, KEY_ARROW_D
+	movia r23, KEY_ARROW_D
 	beq et, r23, _p2_key_arrow_d
 	
-	movui r23, KEY_ARROW_R
+	movia r23, KEY_ARROW_R
 	beq et, r23, _p2_key_arrow_r
 
 	br _eret	
+
+_key_enter:
+	movi r4, 1
+	br _eret
 
 _p1_key_a:
 	movia r18, MOVE_LEFT
@@ -190,11 +226,7 @@ _start:
 
 _game:
 
-	#call _splashscreen					# CREATE STARTSCREEN
-	br _game_start
-	br _game 							# GAME OVER: RESTART
-
-
+	br _splashscreen					# CREATE STARTSCREEN
 
 _setup:
 	
@@ -213,6 +245,9 @@ _setup:
 	stwio r4, PS2_CONTROL(r9)			# PS2	(IRQ 7)
 	movui r4, 0x80
 	wrctl ctl3, r4						# CPU	(ienable)
+
+	movia r4, LED_BASE					# RESETTING LEDs
+	stwio r0, 0(r4)
 	
 	# COLOUR SETUP
 	movia r3, COLOUR_BORDER				# r3 = BORDER
@@ -227,36 +262,124 @@ _setup:
 	movia r16, VGA_BACK_BUFFER 			# r16 = BACK BUFFER MEMORY LOCATION
 	movia r17, VGA_BACK_BUFFER_END		# r17 = END OF BACK BUFFER MEMORY LOCATION
 
-	# PLAYER MOVEMENT STARTING VALUES
-	movia r18, MOVE_RIGHT				# r18 = PLAYER 1 MOVEMENT
-	movia r19, MOVE_LEFT				# r19 = PLAYER 2 MOVEMENT
-
 	# PLAYER STARTING POSITIONS
 	movia r20, P1_START_COORDS			# r20 = PLAYER 1 STARTING COORDINATES
 	movia r21, P2_START_COORDS			# r21 = PLAYER 2 STARTING COORDINATES
 
+    # HEX DISPLAY SETUP
+    movia r22, HEX_ADDRESS              # r22 = HEX DISPLAY ADDRESS
 	ret
 	
 
 
-_splashscreen:							# START-UP SPLASHSCREEN
-										# IMMEDIATELY RESET BYTE IN MEMORY TO ' '
-										# CHECK BYTE IN MEMORY FOR BRANCH TO GAME START
-	br _splashscreen					
-
-
-	### GAME ###
-
-
-_game_start:							# GAME START
+_splashscreen:							
 	
-	# SETUP FOR GAME
+	movi r2, 1							# r2 = SWAP REGISTER (0x1)
+	call _draw_background				# DRAW BACKGROUND
+	
+	movia r4, VGA_CHAR_BUFFER			# DRAW CHARACTERS
+	addi r4, r4, 0x1694
+	movi r5, 0x50 						# "P"
+	sthio r5, 0(r4)
+	movi r5, 0x52						# "R"
+	sthio r5, 2(r4)
+	movi r5, 0x45						# "E"
+	sthio r5, 4(r4)
+	movi r5, 0x53						# "S"
+	sthio r5, 6(r4)
+	movi r5, 0x53						# "S"
+	sthio r5, 8(r4)
+	movi r5, 0x20						# " "
+	sthio r5, 10(r4)
+	movi r5, 0x45						# "E"
+	sthio r5, 12(r4)
+	movi r5, 0x4E						# "N"
+	sthio r5, 14(r4)
+	movi r5, 0x54						# "T"
+	sthio r5, 16(r4)
+	movi r5, 0x45						# "E"
+	sthio r5, 18(r4)
+	movi r5, 0x52						# "R"
+	sthio r5, 20(r4)
+	movi r5, 0x20						# " "
+	sthio r5, 22(r4)
+	movi r5, 0x54						# "T"
+	sthio r5, 24(r4)
+	movi r5, 0x4F						# "O"
+	sthio r5, 26(r4)
+	movi r5, 0x20						# " "
+	sthio r5, 28(r4)
+	movi r5, 0x53						# "S"
+	sthio r5, 30(r4)
+	movi r5, 0x54						# "T"
+	sthio r5, 32(r4)
+	movi r5, 0x41						# "A"
+	sthio r5, 34(r4)
+	movi r5, 0x52						# "R"
+	sthio r5, 36(r4)
+	movi r5, 0x54						# "T"
+	sthio r5, 38(r4)
 
-<<<<<<< HEAD
+	add r4, r0, r0 						# SET r4 TO 0
+
+_wait_for_enter:
+	
+	beq r4, r0, _wait_for_enter
+
+_erase_chars:
+	
+	movia r4, VGA_CHAR_BUFFER			# ERASE CHARACTERS
+	addi r4, r4, 0x1694
+	movi r5, 0x20						# " "
+	sthio r5, 0(r4)
+	movi r5, 0x20						# " "
+	sthio r5, 2(r4)
+	movi r5, 0x20						# " "
+	sthio r5, 4(r4)
+	movi r5, 0x20						# " "
+	sthio r5, 6(r4)
+	movi r5, 0x20						# " "
+	sthio r5, 8(r4)
+	movi r5, 0x20						# " "
+	sthio r5, 10(r4)
+	movi r5, 0x20						# " "
+	sthio r5, 12(r4)
+	movi r5, 0x20						# " "
+	sthio r5, 14(r4)
+	movi r5, 0x20						# " "
+	sthio r5, 16(r4)
+	movi r5, 0x20						# " "
+	sthio r5, 18(r4)
+	movi r5, 0x20						# " "
+	sthio r5, 20(r4)
+	movi r5, 0x20						# " "
+	sthio r5, 22(r4)
+	movi r5, 0x20						# " "
+	sthio r5, 24(r4)
+	movi r5, 0x20						# " "
+	sthio r5, 26(r4)
+	movi r5, 0x20						# " "
+	sthio r5, 28(r4)
+	movi r5, 0x20						# " "
+	sthio r5, 30(r4)
+	movi r5, 0x20						# " "
+	sthio r5, 32(r4)
+	movi r5, 0x20						# " "
+	sthio r5, 34(r4)
+	movi r5, 0x20						# " "
+	sthio r5, 36(r4)
+	movi r5, 0x20						# " "
+	sthio r5, 38(r4)
+
+	br _game_start
+
+_draw_background:	
+
+	movia r14, VGA_FRONT_BUFFER 		# RE-INSTANTIATE r14 TO FRONT BUFFER
+	movia r16, VGA_BACK_BUFFER 			# RE-INSTANTIATE r16 TO BACK BUFFER
+
 	add r4, r0, r0
 	addi r5, r0, 16
-=======
->>>>>>> 15dd97ae1b48506a87ce8559a8621c9b0cb99585
 
 _fill_background:
 
@@ -267,7 +390,7 @@ _fill_background:
 
 	sth r10, 0(r16)						# FILL BACK BUFFER
 	addi r16, r16, 2
-	
+
 	addi r4, r4, 1
 
 	ble r14, r15, _fill_background		# LOOP UNTIL FILLED
@@ -340,17 +463,36 @@ _draw_border_right:
 	addi r16, r16, 1024
 
 	bne r14, r4, _draw_border_right
-	
-	# BORDER DRAWING FINISHED
 
 	movia r14, VGA_FRONT_BUFFER 		# RE-INSTANTIATE r14 TO FRONT BUFFER
 	movia r16, VGA_BACK_BUFFER 			# RE-INSTANTIATE r16 TO BACK BUFFER
 
-	movi r2, 1							# r2 = SWAP REGISTER (0x1)
-
 	stwio r14, VCB_BACK_BUFFER(r8)		# STORING BACKGROUND TO BACK BUFFER
 	stwio r2, VCB_FRONT_BUFFER(r8)		# SWAPPING BUFFERS
 	stwio r16, VCB_BACK_BUFFER(r8)		# STORING BACKGROUND TO BACK BUFFER
+
+	ret
+
+
+	### GAME ###
+
+
+_game_start:							# GAME START
+	
+	# SETUP FOR GAME
+
+_set_score_to_zero_on_hex_display:
+
+    movia r4, SCORE_ZERO_ZERO
+    stwio r4, 0(r22)                    # INITIAL SCORE IS ZERO, ZERO
+
+_game_continue:
+
+	# PLAYER MOVEMENT STARTING VALUES
+	movia r18, MOVE_RIGHT				# r18 = PLAYER 1 MOVEMENT
+	movia r19, MOVE_LEFT				# r19 = PLAYER 2 MOVEMENT
+
+	call _draw_background				# DRAWING BACKGROUND
 
 	mov r6, r14							# COPY OF BUFFERS
 	mov r7, r16							# FOR PLAYER 2
@@ -414,19 +556,313 @@ _check_same_frame_collision:
 	br _same_frame_collision 			# COLLISION OCCURED FOR BOTH PLAYERS AT EXACT SAME FRAME
 	
 	
-_collision_p1:							# PLAYER 1 COLLISION OCCURED
+_collision_p2:							# PLAYER 1 COLLISION OCCURED
 
-	# CHANGE SCORE ON HEX DISPLAY
-	# CHECK IF PERSON HAS REACHED 3 POINTS
-	# IF SO: WINNER SCREEN, BUTTON PRESSED = ret (BACK TO _game)
-	# ELSE: WAIT FOR BUTTON PRESS, br _game_start
+    ldhio r4, 0(r22)
+    andi r4, r4, 0xFFFF
 
-_collision_p2:							# PLAYER 2 COLLISION OCCURED
+    # PLAYER2 SCORE IS ZERO
+    movia r5, SCORE_ZERO_ZERO
+    beq r4, r5, _score_one_zero_p1
 
+    movia r5, SCORE_ONE_ZERO
+    beq r4, r5, _score_two_zero_p1
+
+    movia r5, SCORE_TWO_ZERO
+    beq r4, r5, _score_three_zero_player1_wins
+
+    # PLAYER2 SCORE IS ONE
+    movia r5, SCORE_ZERO_ONE
+    beq r4, r5, _score_one_one_p1
+
+    movia r5, SCORE_ONE_ONE
+    beq r4, r5, _score_two_one_p1
+
+    movia r5, SCORE_TWO_ONE
+    beq r4, r5, _score_three_one_player1_wins
+
+    # PLAYER2 SCORE IS TWO
+    movia r5, SCORE_ZERO_TWO
+    beq r4, r5, _score_one_two_p1
+
+    movia r5, SCORE_ONE_TWO
+    beq r4, r5, _score_two_two_p1
+
+    movia r5, SCORE_TWO_TWO
+    beq r4, r5, _score_three_two_player1_wins
+
+# PLAYER2 SCORE IS ZERO
+_score_one_zero_p1:
+    movia r4, SCORE_ONE_ZERO
+    stwio r4, 0(r22)
+    br _game_continue
+
+_score_two_zero_p1:
+    movia r4, SCORE_TWO_ZERO
+    stwio r4, 0(r22)
+    br _game_continue
+
+_score_three_zero_player1_wins:
+    movia r4, SCORE_THREE_ZERO
+    stwio r4, 0(r22)
+    br _p1_wins
+
+# PLAYER2 SCORE IS ONE
+_score_one_one_p1:
+    movia r4, SCORE_ONE_ONE
+    stwio r4, 0(r22)
+    br _game_continue
+
+_score_two_one_p1:
+    movia r4, SCORE_TWO_ONE
+    stwio r4, 0(r22)
+    br _game_continue
+
+_score_three_one_player1_wins:
+    movia r4, SCORE_THREE_ONE
+    stwio r4, 0(r22)
+    br _p1_wins
+
+# PLAYER2 SCORE IS TWO
+_score_one_two_p1:
+    movia r4, SCORE_ONE_TWO
+    stwio r4, 0(r22)
+    br _game_continue
+
+_score_two_two_p1:
+    movia r4, SCORE_ONE_TWO
+    stwio r4, 0(r22)
+    br _game_continue
+
+_score_three_two_player1_wins:
+    movia r4, SCORE_THREE_TWO
+    stwio r4, 0(r22)
+    br _p1_wins
+
+_collision_p1:							# PLAYER 2 COLLISION OCCURED
+
+    ldhio r4, 0(r22)
+    andi r4, r4, 0xFFFF
+
+    # PLAYER1 SCORE IS ZERO
+    movia r5, SCORE_ZERO_ZERO
+    beq r4, r5, _score_zero_one_p2
+
+    movia r5, SCORE_ZERO_ONE
+    beq r4, r5, _score_zero_two_p2
+
+    movia r5, SCORE_ZERO_TWO
+    beq r4, r5, _score_zero_three_player2_wins
+
+    # PLAYER1 SCORE IS ONE
+    movia r5, SCORE_ONE_ZERO
+    beq r4, r5, _score_one_one_p2
+
+    movia r5, SCORE_ONE_ONE
+    beq r4, r5, _score_one_two_p2
+
+    movia r5, SCORE_ONE_TWO
+    beq r4, r5, _score_one_three_player2_wins
+
+    # PLAYER1 SCORE IS TWO
+    movia r5, SCORE_TWO_ZERO
+    beq r4, r5, _score_two_one_p2
+
+    movia r5, SCORE_TWO_ONE
+    beq r4, r5, _score_two_two_p2
+
+    movia r5, SCORE_TWO_TWO
+    beq r4, r5, _score_two_three_player2_wins
+
+# PLAYER2 SCORE IS ZERO
+_score_zero_one_p2:
+    movia r4, SCORE_ZERO_ONE
+    stwio r4, 0(r22)
+    br _game_continue
+
+_score_zero_two_p2:
+    movia r4, SCORE_ZERO_TWO
+    stwio r4, 0(r22)
+    br _game_continue
+
+_score_zero_three_player2_wins:
+    movia r4, SCORE_ZERO_THREE
+    stwio r4, 0(r22)
+    br _p2_wins
+
+# PLAYER2 SCORE IS ONE
+_score_one_one_p2:
+    movia r4, SCORE_ONE_ONE
+    stwio r4, 0(r22)
+    br _game_continue
+
+_score_one_two_p2:
+    movia r4, SCORE_ONE_TWO
+    stwio r4, 0(r22)
+    br _game_continue
+
+_score_one_three_player2_wins:
+    movia r4, SCORE_ONE_THREE
+    stwio r4, 0(r22)
+    br _p2_wins
+
+# PLAYER2 SCORE IS TWO
+_score_two_one_p2:
+    movia r4, SCORE_TWO_ONE
+    stwio r4, 0(r22)
+    br _game_continue
+
+_score_two_two_p2:
+    movia r4, SCORE_TWO_TWO
+    stwio r4, 0(r22)
+    br _game_continue
+
+_score_two_three_player2_wins:
+    movia r4, SCORE_TWO_THREE
+    stwio r4, 0(r22)
+    br _p2_wins
 
 _same_frame_collision:
 	
+	br _game_continue
+
+_p1_wins:
 	
+	movia r4, VGA_CHAR_BUFFER			# DRAW CHARACTERS
+	addi r4, r4, 0x1696
+	movi r5, 0x50 						# "P"
+	sthio r5, 0(r4)
+	movi r5, 0x4C						# "L"
+	sthio r5, 2(r4)
+	movi r5, 0x41						# "A"
+	sthio r5, 4(r4)
+	movi r5, 0x59						# "Y"
+	sthio r5, 6(r4)
+	movi r5, 0x45						# "E"
+	sthio r5, 8(r4)
+	movi r5, 0x52						# "R"
+	sthio r5, 10(r4)
+	movi r5, 0x20						# " "
+	sthio r5, 12(r4)
+	movi r5, 0x4F						# "O"
+	sthio r5, 14(r4)
+	movi r5, 0x4E						# "N"
+	sthio r5, 16(r4)
+	movi r5, 0x45						# "E"
+	sthio r5, 18(r4)
+	movi r5, 0x20						# " "
+	sthio r5, 20(r4)
+	movi r5, 0x57						# "W"
+	sthio r5, 22(r4)
+	movi r5, 0x49						# "I"
+	sthio r5, 24(r4)
+	movi r5, 0x4E						# "N"
+	sthio r5, 26(r4)
+	movi r5, 0x53						# "S"
+	sthio r5, 28(r4)
+	movi r5, 0x21						# "!"
+	sthio r5, 30(r4)
+
+	add r4, r0, r0 						# SET r4 TO 0
+	br _game_end
+
+_p2_wins:
+	
+	movia r4, VGA_CHAR_BUFFER			# DRAW CHARACTERS
+	addi r4, r4, 0x1696
+	movi r5, 0x50 						# "P"
+	sthio r5, 0(r4)
+	movi r5, 0x4C						# "L"
+	sthio r5, 2(r4)
+	movi r5, 0x41						# "A"
+	sthio r5, 4(r4)
+	movi r5, 0x59						# "Y"
+	sthio r5, 6(r4)
+	movi r5, 0x45						# "E"
+	sthio r5, 8(r4)
+	movi r5, 0x52						# "R"
+	sthio r5, 10(r4)
+	movi r5, 0x20						# " "
+	sthio r5, 12(r4)
+	movi r5, 0x54						# "T"
+	sthio r5, 14(r4)
+	movi r5, 0x57						# "W"
+	sthio r5, 16(r4)
+	movi r5, 0x4F						# "O"
+	sthio r5, 18(r4)
+	movi r5, 0x20						# " "
+	sthio r5, 20(r4)
+	movi r5, 0x57						# "W"
+	sthio r5, 22(r4)
+	movi r5, 0x49						# "I"
+	sthio r5, 24(r4)
+	movi r5, 0x4E						# "N"
+	sthio r5, 26(r4)
+	movi r5, 0x53						# "S"
+	sthio r5, 28(r4)
+	movi r5, 0x21						# "!"
+	sthio r5, 30(r4)
+
+	add r4, r0, r0 						# SET r4 TO 0
+
 _game_end:								# GAME FINISHED
 	
-	br _game_end						# WAIT FOR BUTTON PRESS
+	movia r5, LED_BASE
+	add r2, r0, r0
+
+_led_flicker:
+
+	ldwio r2, 0(r5)
+	nor r2, r2, r0
+	stwio r2, 0(r5)
+	add r2, r0, r0
+	addi r2, r2, 100
+
+_inf_loop:
+
+	subi r2, r2, 1
+	beq r2, r0, _led_flicker
+	beq r4, r0, _inf_loop
+
+	stwio r0, 0(r5)
+
+	movia r4, VGA_CHAR_BUFFER			# ERASE CHARACTERS
+	addi r4, r4, 0x1694
+	movi r5, 0x20						# " "
+	sthio r5, 0(r4)
+	movi r5, 0x20						# " "
+	sthio r5, 2(r4)
+	movi r5, 0x20						# " "
+	sthio r5, 4(r4)
+	movi r5, 0x20						# " "
+	sthio r5, 6(r4)
+	movi r5, 0x20						# " "
+	sthio r5, 8(r4)
+	movi r5, 0x20						# " "
+	sthio r5, 10(r4)
+	movi r5, 0x20						# " "
+	sthio r5, 12(r4)
+	movi r5, 0x20						# " "
+	sthio r5, 14(r4)
+	movi r5, 0x20						# " "
+	sthio r5, 16(r4)
+	movi r5, 0x20						# " "
+	sthio r5, 18(r4)
+	movi r5, 0x20						# " "
+	sthio r5, 20(r4)
+	movi r5, 0x20						# " "
+	sthio r5, 22(r4)
+	movi r5, 0x20						# " "
+	sthio r5, 24(r4)
+	movi r5, 0x20						# " "
+	sthio r5, 26(r4)
+	movi r5, 0x20						# " "
+	sthio r5, 28(r4)
+	movi r5, 0x20						# " "
+	sthio r5, 30(r4)
+	movi r5, 0x20						# " "
+	sthio r5, 32(r4)
+	movi r5, 0x20
+
+	movi r2, 1
+	br _game							# RESTART
